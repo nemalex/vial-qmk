027753b02d5e68cb3059fd50852528ef55afa2dc
nemalex <git@nemalex.xyz>
2025-01-05 17:31:53 +0100

    feat: added butterboard ortho40

---
+++ keyboards/butterboard/ortho40/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xFF, 0x8D, 0x8D, 0xD1, 0xDA, 0xC6, 0x21, 0xE4}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
+#define VIAL_UNLOCK_COMBO_COLS {0, 1}

8e5d302f66bbd9125f37987b1b73f5aaa6076eb6
nemalex <git@nemalex.xyz>
2025-05-07 20:36:15 +0200

    feat: added paraortho

---
+++ keyboards/handwired/paraortho/5x12/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x1B, 0xE2, 0xBE, 0x79, 0x46, 0x7D, 0x2B, 0xED}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
+#define DYNAMIC_KEYMAP_LAYER_COUNT 4
